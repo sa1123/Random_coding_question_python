5ed49de81871bc12b1bfeeccdb1a3060f4acef10
sa1123 <51375919+sa1123@users.noreply.github.com>
2022-03-12 16:59:19 +0530

    Update add_digits.py

--- add_digits.py
+++ add_digits.py
@@ -1,3 +1,6 @@
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+# https://leetcode.com/problems/add-digits/
+
 from audioop import add
 import math
 
