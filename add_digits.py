c4c10dbd9fe9f1bedb6b2e5c1ef5e177d07c207d
sa1123 <51375919+sa1123@users.noreply.github.com>
2022-03-12 16:57:56 +0530

    Add files via upload

---
+++ add_digits.py
@@ -0,0 +1,16 @@
+from audioop import add
+import math
+
+def add_digits(num):
+    if(len(str(num)) == 1):
+        return num
+    sum_of_digits = 0
+    while(num>0):
+        sum_of_digits += num%10
+        num = math.floor(num/10)
+    if(len(str(sum_of_digits)) == 1):
+        return sum_of_digits
+    else:
+        return add_digits(sum_of_digits)
+
+print(add_digits(9999999999999))
